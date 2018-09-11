fe3db6b10177b04972d463bf951e5b095155d225
Axel Haustant <noirbizarre@gmail.com>
2018-09-11 14:28:05 +0200

    Fix production URLs

--- publishconf.py
+++ publishconf.py
@@ -13,9 +13,16 @@ FEED_ALL_ATOM = 'feeds/all.atom'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom'
 
 ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+
 CATEGORY_URL = 'blog/category/{slug}/'
+CATEGORY_SAVE_AS = 'blog/category/{slug}/index.html'
+
 TAG_URL = 'blog/tag/{slug}/'
+TAG_SAVE_AS = 'blog/tag/{slug}/index.html'
+
 PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}/index.html'
 
 
 DELETE_OUTPUT_DIRECTORY = True
